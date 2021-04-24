3fe44ccb64a6eae7fdcf6c6bdba26f062fecd510
翔 张 <2457210773@qq.com>
2021-04-24 22:21:57 +0800

    代码测试

---
+++ test.c
@@ -0,0 +1,51 @@
+#include "contact.h"
+
+void menu()
+{
+	printf("**************************************\n");
+	printf("*****    1.add          2.del    *****\n");
+	printf("*****    3.search       4.modify *****\n");
+	printf("*****    5.show         6.sort   *****\n");
+	printf("*****    0.exit                  *****\n");
+	printf("**************************************\n");
+}
+int main()
+{
+	int input = 0;
+	struct Contact con;
+	InitContact(&con);
+	do
+	{
+		menu();
+		printf("请选择你需要的操作:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case DEL:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			break;
+		case MODIFY:
+			break;
+		case SHOW:
+			ShowContact(&con);
+			break;
+		case SORT:
+			break;
+		case EXIT:
+			printf("退出程序\n");
+			break;
+		default:
+			printf("输入错误，请重新输入\n");
+			break;
+		}
+
+
+	} while (input);
+	system("pause");
+	return 0;
+}
