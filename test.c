2ff1bb523206f3f62907d7dc36cf6bce2b698d18
翔 张 <2457210773@qq.com>
2021-04-28 21:28:08 +0800

    动态通讯录.c文件

--- test.c
+++ test.c
@@ -6,7 +6,7 @@ void menu()
 	printf("*****    1.add          2.del    *****\n");
 	printf("*****    3.search       4.modify *****\n");
 	printf("*****    5.show         6.sort   *****\n");
-	printf("*****    0.exit                  *****\n");
+	printf("*****    7.save         0.exit   *****\n");
 	printf("**************************************\n");
 }
 int main()
@@ -28,8 +28,10 @@ int main()
 			DelContact(&con);
 			break;
 		case SEARCH:
+			SearchContact(&con);
 			break;
 		case MODIFY:
+			ModifyContact(&con);
 			break;
 		case SHOW:
 			ShowContact(&con);
@@ -37,7 +39,10 @@ int main()
 		case SORT:
 			break;
 		case EXIT:
+			Release(&con);
 			printf("退出程序\n");
+			break;
+		case SAVE:			
 			break;
 		default:
 			printf("输入错误，请重新输入\n");
