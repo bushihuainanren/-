05c92298afe6849049b9258c756856db7057bee3
翔 张 <2457210773@qq.com>
2021-05-06 21:29:45 +0800

    消失的数字

--- test.c
+++ test.c
@@ -1,56 +1,23 @@
-#include "contact.h"
-
-void menu()
-{
-	printf("**************************************\n");
-	printf("*****    1.add          2.del    *****\n");
-	printf("*****    3.search       4.modify *****\n");
-	printf("*****    5.show         6.sort   *****\n");
-	printf("*****    7.save         0.exit   *****\n");
-	printf("**************************************\n");
-}
+//消失的数字
+#include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-	int input = 0;
-	struct Contact con;
-	InitContact(&con);
-	do
+	int arr[10] = { 1,2,3,4,5,7,8,9,10 };//给一个缺了一个字的数组
+	int sum = 10;//数组的总个数
+	int arr1[10] = { 0 };//给定一个有序的数组
+	int tmp = 0;//把缺了数字的数组1异或出来的值存起来
+	int tmp1 = 0;//储存有序数组异或出来的值
+	for (int i = 0; i < sum; i++)
 	{
-		menu();
-		printf("请选择你需要的操作:>");
-		scanf("%d", &input);
-		switch (input)
-		{
-		case ADD:
-			AddContact(&con);
-			break;
-		case DEL:
-			DelContact(&con);
-			break;
-		case SEARCH:
-			SearchContact(&con);
-			break;
-		case MODIFY:
-			ModifyContact(&con);
-			break;
-		case SHOW:
-			ShowContact(&con);
-			break;
-		case SORT:
-			break;
-		case EXIT:
-			Release(&con);
-			printf("退出程序\n");
-			break;
-		case SAVE:			
-			break;
-		default:
-			printf("输入错误，请重新输入\n");
-			break;
-		}
-
-
-	} while (input);
-	system("pause");
+		tmp ^= arr[i];
+	}
+	for (int i = 0; i <= sum; i++)
+	{
+		tmp1 ^= i;
+	}
+	tmp ^= tmp1;//相互异或得到的就是第二个数组比第一个数组多出来的那个字
+	printf("%d", tmp);
+	system("pause>0");
 	return 0;
 }
